099a24f62df8398c12ebc7c1a5baa1c57e4be4c9
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 02:54:51 +0700

    6.13 Destructing and Property Shorthand Challenge

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=95c0f144604c71340115574a6812417a&units=metric'
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!')
-        } else if (response.body.message) {
-            callback(response.body.message)
+        } else if (body.message) {
+            callback(body.message)
         } else {
-            const current = response.body.current
+            const current = body.current
             ans = current.weather[0].description + '. It is currently ' + current.temp + ' degrees out. There is a ' + current.humidity + ' Humidity.'
             callback(undefined, ans)
         }
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,18 +2,19 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaGFpdHVhbjEzNCIsImEiOiJja3RoMWxxemMwbm5lMnFubDl1enJvcWxtIn0.5tKZ5bI6WQGtJvxPLT8n_g&limit=1'
-    request({url: url, json:true}, (error, response) => {
+    
+    request({url, json:true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to geo service!', undefined)
-        } else if (response.body.message) {
-            callback(response.body.message, undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.message) {
+            callback(body.message, undefined)
+        } else if (body.features.length === 0) {
             callback('No search result.', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name,
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name,
             })
         }
     })
