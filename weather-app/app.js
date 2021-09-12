826aaaf0dd450fef94072428df73c09c451f3f19
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 02:29:33 +0700

    6.11 Callback Chainning

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,12 +2,22 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('huế việt nam', (error, data) => {
-    console.log(error)
-    console.log(data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
+if (!address) {
+    console.log('Please provide an address.')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        } 
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
