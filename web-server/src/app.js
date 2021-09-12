f9f661f5e3c65d9e3117f92c30ddd741dd291478
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 05:06:26 +0700

    7.6 Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,32 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather",
+        name: "Hai Tuan", 
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me', 
+        name: 'Hai Tuan',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help', 
+        name: 'Hai Tuan',
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
