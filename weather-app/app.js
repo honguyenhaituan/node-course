07948968790a3855df3427db1d9254da465a7a8e
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-12 16:53:22 +0700

    6.5 Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=0.123&lon=35.123&appid=95c0f144604c71340115574a6812417a'
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=0.123&lon=35.123&appid=95c0f144604c71340115574a6812417a&units=metric&lang=vi'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({url: url, json: true}, (error, response) => {
+    const current = response.body.current
+    console.log('It is currently ' + current.temp + ' degrees out.')
+    console.log('There is a ' + current.rain['1h'] + ' chance of rain.')
+    console.log(response.body.daily[0])
 })
