5b56c86a9983718f7e3f43afa1b8ea704903b8eb
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-12 09:34:13 +0700

    3.5 Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,7 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 console.log(getNotes())
-
 console.log(validator.isURL('haituan134.com'))
+console.log(chalk.green('Success!'))
