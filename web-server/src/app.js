19133c61734fdfc5e638f944b0f0a18cf580b4c0
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 03:45:32 +0700

    7.4 Serving p static asset

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,12 @@
+const path = require('path')
 const express = require('express')
 
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
-
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send('<h1>Help</h1>')
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     res.send({
