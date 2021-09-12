bdbc333cba79e0168833839a3d38012f41044f19
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 03:30:19 +0700

    7.3 Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,22 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page!')
+    res.send('<h1>Help</h1>')
 })
 
 app.get('/about', (req, res) => {
-    res.send('About!')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page!')
+    res.send({
+        forecast: "mot hai ba", 
+        location: "bon nam sau",
+    })
 })
 
 app.listen(3000, () => {
