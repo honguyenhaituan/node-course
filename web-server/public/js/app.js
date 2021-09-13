7209e058039b7ea78960f69044a9cef80b08a9ba
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 13:29:27 +0700

    8.7 Writinng up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,18 +3,22 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    messageOne.textContent = 'loading...'
+    messageTwo.textContent = ''
 
     const location = search.value
-
     fetch('/weather?address=' + encodeURIComponent(location)).then((res) => {
         res.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
