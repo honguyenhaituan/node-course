ec47208bf762d62b2f2d30b46b1d3cc0e7e451e6
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 11:11:18 +0700

    8.2 The Query String

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,9 +39,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide a address.'
+        })
+    }
+
     res.send({
         forecast: "mot hai ba", 
         location: "bon nam sau",
+        address: req.query.address,
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+
+    res.send({
+        products: [],
     })
 })
 
