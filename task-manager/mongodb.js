c8445435c1ba257ea604713a6f8fff6b377c752b
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-14 03:32:54 +0700

    10.7 Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,8 +12,48 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     const db = client.db(databaseName)
 
 
-    db.collection('users').insertOne({
-        name: 'Hai Tuan',
-        age: 22,
+    // db.collection('users').insertOne({
+    //     name: 'Hai Tuan',
+    //     age: 22,
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28,
+    //     }, {
+    //         name: 'Gunther', 
+    //         age: 28,
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.insertedCount)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            decription: "mot",
+            completed: true,
+        }, {
+            decription: "hai", 
+            completed: false,
+        }, {
+            decription: "ba", 
+            completed: true,
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable connect to server')
+        }
+        console.log(result.insertedCount)
     })
 })
