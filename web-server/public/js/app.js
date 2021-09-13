ce04f9623f5666affe980ccb2d07761ba89275e3
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 11:40:21 +0700

    8.5 Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('/weather?address=Boston').then((res) => {
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data)
+        }
+    })
+})
