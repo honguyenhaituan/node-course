1f6520e4cb58f998c698d46e998135015d586d99
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-13 12:26:48 +0700

    8.6 Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,21 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('/weather?address=Boston').then((res) => {
-    res.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data)
-        }
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('/weather?address=' + encodeURIComponent(location)).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data)
+            }
+        })
     })
 })
