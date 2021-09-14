b0b9324776a14e574cd87a52fa58e436a728d32b
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-14 17:30:30 +0700

    11.19 Resource Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -51,7 +51,7 @@ app.patch('/users/:id', async (req, res) => {
     if (!isValidOperation) {
         return res.status(400).send({error: 'Invalid updates'})
     }
-    
+
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {
             new: true, 
@@ -66,6 +66,19 @@ app.patch('/users/:id', async (req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
 })
 
 app.post('/tasks', async (req, res) => {
@@ -98,6 +111,43 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['decription', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, 
+            runValidators: true
+        })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 
