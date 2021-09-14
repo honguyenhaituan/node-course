6ce46be67d737412f2885da48f15730bf65b355e
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-14 14:46:21 +0700

    11.8 Resource Creation Endpoints Part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,57 +1,7 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    password: {
-        type: String, 
-        minLength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password doesn\'t contain \"password\"')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid.')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '   Hai Tuan  ',
-//     email: 'HAITUAn@goole.com   ',
-//     password: 'haituan1234paSsword123'
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error ', error)
-// })
 
 
 
@@ -65,14 +15,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false,
     }
-})
-
-const task = new Task({
-    decription: "Home work   ",
-})
-
-task.save().then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
 })
