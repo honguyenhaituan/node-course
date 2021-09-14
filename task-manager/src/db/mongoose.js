24c411ff4a2f11f7a04019e257568f640015e426
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-14 13:34:09 +0700

    11.4 Data validation and Sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,43 +1,63 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid.')
+            }
+        }
     },
     age: {
         type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Hai Tuan',
-//     age: "Mike",
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error ', error)
-// })
-
-const Task = mongoose.model('Task', {
-    decription: {
-        type: String,
-    }, 
-    completed: {
-        type: Boolean,
-    }
+const me = new User({
+    name: '   Hai Tuan  ',
+    email: 'HAITUAn@goole.com   ',
 })
 
-const task = new Task({
-    decription: "Home work",
-    completed: false,
-})
-
-task.save().then((result) => {
-    console.log(result)
+me.save().then((me) => {
+    console.log(me)
 }).catch((error) => {
-    console.log(error)
+    console.log('Error ', error)
 })
+
+// const Task = mongoose.model('Task', {
+//     decription: {
+//         type: String,
+//     }, 
+//     completed: {
+//         type: Boolean,
+//     }
+// })
+
+// const task = new Task({
+//     decription: "Home work",
+//     completed: false,
+// })
+
+// task.save().then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
