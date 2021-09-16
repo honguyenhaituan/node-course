9695b5611e82d76b8ef05f1157b977d719de4665
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-16 16:09:53 +0700

    12.14 Authenticating Task Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,17 +14,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-const main = async () => {
-    // const task = await Task.findById('614300245a3f33dfb4f557a0')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    const user = await User.findById('6142ff62d397b0c222455590')
-    await user.populate('tasks')
-    console.log(user.tasks)
-}
-
-main()
