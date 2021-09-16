451f1a100c7ef650e10b6e5cb6f377b4a84e3554
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-16 11:38:07 +0700

    12.7 Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,9 +7,25 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.port || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Maintenance server')
+})
+
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
