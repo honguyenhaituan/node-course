5eb3bc142c8c128dde0cd840e5374dc789589a27
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-16 14:01:53 +0700

    12.8Accepting Authentication Token

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,20 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.port || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('Maintenance server')
-})
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
