c655b60d405199744a8bc64cca65efb0b0ace02b
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-09-16 14:55:36 +0700

    12.11 Hiding Private Data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,20 +11,8 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123'}, 'Thisismynewcoures', { expiresIn: '7 days'})
-    console.log(token)
-
-    const data = jwt.verify(token, 'Thisismynewcoures')
-    console.log(data)
-}
-
-myFunction()
